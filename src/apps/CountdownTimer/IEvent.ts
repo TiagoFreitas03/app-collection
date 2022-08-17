fb09ef12256838988dea38e73ac1d9a277dc1601
Tiago Freitas <tiago.freitas.faria@gmail.com>
2022-08-17 08:29:07 -0300

    ajustes na aplicação Countdown Timer

---
+++ src/apps/CountdownTimer/IEvent.ts
@@ -0,0 +1,10 @@
+export interface IEvent {
+	// nome do evento
+	name: string
+
+	// data do evento formatada como string
+	date: string
+
+	// data e hora do evento em ms
+	timestamp: number
+}
--- src/apps/CountdownTimer/index.tsx
+++ src/apps/CountdownTimer/index.tsx
@@ -1,15 +1,9 @@
 import { useState, FormEvent, useEffect } from 'react'
 
-import { isDate, isTime } from '../utils/validations'
-import { dateToString, stringToDate, datetimeToString } from '../utils/convertions'
-
-interface IEvent {
-	name: string
-	date: string
-	timestamp: number
-}
-
-const TITLES = ['Dias', 'Horas', 'Minutos', 'Segundos']
+import { Sidebar } from './Sidebar'
+import { IEvent } from './IEvent'
+import { isDate, isTime } from '../../utils/validations'
+import { dateToString, stringToDate, datetimeToString } from '../../utils/convertions'
 
 export function CountdownTimer() {
 	const [name, setName] = useState('')
@@ -45,10 +39,10 @@ export function CountdownTimer() {
 
 		if (name === '')
 			return alert('Informe o nome do evento')
-		
+
 		if (!isDate(date))
 			return alert('Data inválida')
-		
+
 		if (!isTime(time))
 			return alert('Hora inválida')
 
@@ -73,23 +67,6 @@ export function CountdownTimer() {
 		const aux = events.slice()
 		aux.splice(index, 1)
 		setEvents(aux)
-	}
-
-	function formatRemaining(timestamp: number, pos: number) {
-		const diff = timestamp - now
-
-		if (diff <= 0) {
-			alert(`Evento ${events[pos].name} finalizado`)
-			removeEvent(pos)
-			return [0, 0, 0, 0]
-		}
-
-		const days = Math.floor(diff / (60 * 60 * 24)),
-			hours = Math.floor(diff % (60 * 60 * 24) / (60 * 60)),
-			minutes = Math.floor(diff % (60 * 60) / 60),
-			seconds = Math.floor(diff % 60)
-
-		return [days, hours, minutes, seconds]
 	}
 
 	return (
@@ -131,44 +108,7 @@ export function CountdownTimer() {
 				</form>
 			</main>
 
-			<aside className="h-screen overflow-y-auto w-[45%] min-w-[480px] p-6">
-				<h2 className="mb-4 text-3xl">Eventos Iniciados</h2>
-
-				{events.length > 0 ? (
-					events.map((e, i) => {
-						return (
-							<div
-								key={i}
-								className="border-2 border-gray-500 rounded p-3 my-3 bg-gray-700 flex justify-between"
-							>
-								<div>
-									<div>
-										<h3 className="text-2xl mb-4">{e.name}</h3>
-										<p>{e.date}</p>
-									</div>
-
-									<div className="flex justify-between gap-x-2 mt-5">
-										{formatRemaining(e.timestamp, i).map((x, j) => {
-											return (
-												<span key={j} className="text-center w-[80px]">
-													<h3 className="border-b-4 border-blue-600 mb-1">{x}</h3>
-													<p>{TITLES[j]}</p>
-												</span>
-											)
-										})}
-									</div>
-								</div>
-
-								<div>
-									<button onClick={() => removeEvent(i)}>&times;</button>
-								</div>
-							</div>
-						)
-					})
-				) : (
-					<p>Nenhum evento iniciado...</p>
-				)}
-			</aside>
+			<Sidebar events={events} now={now} removeEvent={(pos) => removeEvent(pos)} />
 		</div>
 	)
 }

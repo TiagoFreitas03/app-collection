22e44f1f7e13cc2a6e2692b5e131d246d574bc62
Tiago Freitas <tiago.freitas.faria@gmail.com>
2022-09-08 08:19:14 -0300

    separação da página Home

---
+++ src/Home.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom'
+
+export function Home() {
+	return (
+		<div>
+			<Link to='/bin-2-dec'>Bin 2 Dec</Link>
+			<Link to='/border-radius-previewer'>Border Radius Previewer</Link>
+			<Link to='/calculator'>Calculator</Link>
+			<Link to='/cause-effect'>Cause Effect</Link>
+			<Link to='/christmas-lights'>Christmas Lights</Link>
+			<Link to='/color-cycle'>Color Cycle</Link>
+			<Link to='/countdown-timer'>Countdown Timer</Link>
+			<Link to='/csv-2-json'>CSV 2 JSON</Link>
+			<Link to='/dollars-to-cents'>Dollars To Cents</Link>
+			<Link to='/dynamic-css-var'>Dynamic CSS Variables</Link>
+			<Link to='/first-db-app'>First DB App</Link>
+			<Link to='/flip-image'>Flip Image</Link>
+			<Link to='/github-status'>GitHub Status</Link>
+			<Link to='/iot-mailbox-simulator'>IOT Mailbox Simulator</Link>
+			<Link to='/js-input-validation'>JS Input Validation</Link>
+			<Link to='/hello'>Hello App</Link>
+			<Link to='/json-2-csv'>JSON 2 CSV</Link>
+			<Link to='/key-value'>Key Value</Link>
+			<Link to='/lorem-ipsum-generator'>Lorem Ipsum Generator</Link>
+			<Link to='/notes'>Notes</Link>
+			<Link to='/quiz'>Quiz</Link>
+			<Link to='/random-meal-generator'>Random Meal Generator</Link>
+			<Link to='/random-number'>Random Number Generator</Link>
+			<Link to='/recipe'>Recipe</Link>
+			<Link to='/roman-to-decimal'>Roman to Decimal</Link>
+			<Link to='/slider-design'>Slider Design</Link>
+			<Link to='/stopwatch'>Stopwatch</Link>
+			<Link to='/true-or-false'>True or False</Link>
+			<Link to='/vigenere-cipher'>Vigenere Cipher</Link>
+			<Link to='/weather'>Weather</Link>
+			<Link to='/wind-chill'>Wind Chill</Link>
+			<Link to='/word-frequency'>Word Frequency</Link>
+		</div>
+	)
+}

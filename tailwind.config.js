f7870486b1a1f458d575836aa9c421ab03ab1de9
Tiago Freitas <tiago.freitas.faria@gmail.com>
2022-09-08 15:50:01 -0300

    configuração da backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,15 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			backgroundImage: {
+        blur: 'url(/src/assets/ocean-background.jpg)'
+      },
 			fontFamily: {
 				sans: 'Roboto, sans-serif',
 				serif: 'Nunito',
 				mono: '"JetBrains Mono"'
 			},
 			colors: {
-				blue: {
-					900: '#03045E',
-					800: '#023E8A',
-					700: '#0077B6',
-					600: '#0096C7',
-					500: '#00B4D8',
-					400: '#48CAE4',
-					300: '#90E0EF',
-					200: '#ADE8F4',
-					100: '#CAF0F8',
-				},
 				gray: {
           900: '#09090A',
           700: '#121214',

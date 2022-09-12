a7a71f36898d27c57aa8c28d314240d567de1f0a
Tiago Freitas <tiago.freitas.faria@gmail.com>
2022-09-12 08:53:39 -0300

    alteração na formatação de ms

--- src/utils/convertions.ts
+++ src/utils/convertions.ts
@@ -33,12 +33,17 @@ export function stringToDate(date: string, time?: string) {
 	return new Date(year, month - 1, day, hours, minutes, seconds)
 }
 
-export function formatMs(time: number) {
+export function formatMs(time: number, includeMs: boolean = true) {
 	const ms = Math.floor((time % 1000) / 10),
 		s = (Math.floor((time / 1000) % 60)),
 		m = Math.floor((time / (60000)) % 60)
 
 	const format = (x: number) => x.toString().padStart(2, '0')
 
-	return [format(m), format(s), format(ms)].join(':')
+	let result = [format(m), format(s)].join(':')
+
+	if (includeMs)
+		result += ':' + format(ms)
+
+	return result
 }
